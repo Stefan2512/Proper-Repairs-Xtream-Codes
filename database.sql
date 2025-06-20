ccdb69807c2ac95173493c4169cb673aef0deb7a
Sony2512 <stefan@outlook.ie>
2025-06-20 11:08:07 +0100

    Update database.sql
    updates

--- database.sql
+++ database.sql
@@ -5,7 +5,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
+--
+-- Upgraded Stefan - Edition @ June 2025
+--
 --
 -- Database: `xtream_iptvpro`
 --
